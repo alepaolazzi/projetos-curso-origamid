8f58466d441384216b598e22c492018eef33cc87
alepaolazzi <alepaolazzidasilva@gmail.com>
2022-07-14 18:00:06 -0300

    add javascript e att html/css

--- animais-fantasticos/assets/script.js
+++ animais-fantasticos/assets/script.js
@@ -44,5 +44,55 @@ function initAccordion() {
     });
   }
 }
+
+function initScrollSuave() {
+  const linksInternos = document.querySelectorAll(".js-menu a[href^='#']");
+
+  function scrollToSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute("href");
+    const section = document.querySelector(href);
+    const topo = section.offsetTop;
+    window.scrollTo({
+      top: topo,
+      behavior: "smooth",
+    });
+  }
+  linksInternos.forEach((link) => {
+    link.addEventListener("click", scrollToSection);
+  });
+
+  // Forma alternativa de fazer o scroll suave
+
+  /* section.scrollIntoView({
+    behavior: "smooth",
+    block: "start",
+  });
+     */
+}
+
+function initAnimacaoScroll() {
+  const sections = document.querySelectorAll(".js-scroll");
+  const windowMetade = window.innerHeight * 0.6;
+  if (sections.length) {
+    function animaScroll() {
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top;
+        const isSectionVisible = sectionTop - windowMetade < 0;
+        if (isSectionVisible) {
+          section.classList.add("ativo");
+        } else {
+          section.classList.remove("ativo");
+        }
+      });
+    }
+    animaScroll();
+
+    window.addEventListener("scroll", animaScroll);
+  }
+}
+
 initTabNav();
 initAccordion();
+initScrollSuave();
+initAnimacaoScroll();
